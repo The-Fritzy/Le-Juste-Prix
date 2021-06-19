113c089f519a5ded24cf166ab5fe84bf8a3c2b39
Fritzy <mi3.andreileonard@gmail.com>
2021-06-19 09:24:31 +0200

    Adding Real C++ (Base Code C but with modification of lib and other things)

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,45 +11,46 @@
 
 // Author : A.L Mitroi (Base code but with big modification (French) : https://openclassrooms.com/forum/sujet/le-juste-prix-93210)
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
  
 int main(int argc, char *argv[])
 {
 
     // Define the range of the number to write
     const long range1 = 1, range2 = 15;
-    long number_of_user = 0, juste_prix = 0, gameLoop = 1;
+    long number_of_user = 0, juste_prix = 0, gameLoop = true;
  
     srand (time(NULL));
     juste_prix = (rand() % (range2 - range1 + 1)) + range1;
  
-    printf("Welcome, Into Le Juste Prix ! (French Name of The Game)");
+    cout << "Welcome, Into Le Juste Prix ! (French Name of The Game)";
  
     // Game Loop
-    while ( gameLoop == 1 )
+    while ( gameLoop == true )
     {
-        printf("\nEnter The Number you think is correct between 1 and 15 : ");
+        cout << "\nEnter The Number you think is correct between 1 and 15 : ";
         // Input of the number of user
-        scanf("%ld", &number_of_user);
+        cin >> number_of_user;
 
         // Verify if the number of the user is in the range
         if (number_of_user > range2)
-            printf("ERROR : Number Out of Range");
+            cout << "ERROR : Number Out of Range";
         else {
     
             // If number is too small
             if (juste_prix > number_of_user)
-                printf("Number Too Small");
+                cout << "Number Too Small";
 
             // If number is too large
             else if (juste_prix < number_of_user)
-                printf("Number Too Large");
+                cout << "Number Too Large";
         
             // If number is good
             else if (juste_prix == number_of_user)
-                printf("Well done you found it! the number to guess was : %ld", juste_prix);
+                cout << "Well done you found it! the number to guess was : " << juste_prix;
         }
     }
 
